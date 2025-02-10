869bb04ffd7962984f3f5d5c0783452d3724521a
aobkrit <aobkrit.l@ku.th>
2025-02-10 16:44:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,77 @@
-function submitData () {
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]'); 
-    let ageDOM = document.querySelector('input[name=age]'); 
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked'); 
-    let descriptionDOM = document.querySelector('textarea[name=description]'); 
+const express =require('express');
+const bodyParser =require('body-parser');
+const app = express();
 
-    let interest = ''
-    for (let i = 0; i < interestDOMs.length; i++ ){
-        interest += interestDOMs[i].value
-        if (i != interestDOMs.length - 1){
-            interest += ','
-        }
+app.use(bodyParser.text());
+const port = 8000;
+
+// เก็บ user
+let users = []
+let counter = 1
+
+/*
+GET /users สำหรับ get users ทั้งหมด
+POST /users สำหรับ create user ใหม่ที่บันทึกเข้าไป
+GET /users/
+PUT /users/:id สำหรับ update user รายคนที่ต้องการบันทึกเข้าไป
+
+*/
+// path = GET /users
+app.get('/users',(req,res) => {
+    res.json(users)
+})
+
+// path = POST / user
+app.post('/user',(req,res) => {  
+    let user = req.body;
+    user.id = counter
+    counter +=1
+    users.push(user);
+    res.json({
+        message: "User created" ,
+        user: user
+    });
+})
+
+// path = PUT /user/:id
+app.put('/user/:id',(req,res) => { //ใช้ในการแก้ไขข้อมูล
+    let id = req.params.id;
+    let updateUser = req.body;
+    //หา user จาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id)
+    //update user นั้น
+    if (updateUser.firstname) {
+        users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
     }
-    
-    let userData = {
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age : ageDOM.value,
-        gender : genderDOM.value,
-        description : descriptionDOM.value,
-        interest : interest
-
+    if (updateUser.lastname){
+        users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
     }
-        console.log('submitData',userData);
 
-}
+    res.json({
+        message: "User updated",
+      data: {
+        user: updateUser,
+        indexUpdate: selectedIndex
+      }  
+    });
+    //ส่งข้อมูล user ที่ update ส่งกลับไปเข้าที่เดิม
+  
+})
+
+app.listen(port,(req,res) => {  //ตัวช่วยในการเฝ้าการส่งข้อมูล และส่งข้อมูลออกไป 
+    console.log('Server is running on port' + port);
+});
+
+
+// path = DELETE /user/:id
+app.delete('/user/:id',(req,res) => {
+    let id = req.params.id;
+    //หา index ของ user ที่ต้องการลบ
+    let selectedIndex = users.findIndex(user => user.id == id)
+
+    users.splice(selectedIndex,1)
+    res.json({
+        message: "Delete Completed",
+        indexDeleted: selectedIndex
+    })
+})
